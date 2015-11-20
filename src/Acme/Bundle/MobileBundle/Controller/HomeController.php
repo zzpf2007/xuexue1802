246708daa03af8c6d918597cd167f36bf0617b04
zzpf <zzpf@163.com>
2015-11-20 23:18:02 +0800

    banner

--- src/Acme/Bundle/MobileBundle/Controller/HomeController.php
+++ src/Acme/Bundle/MobileBundle/Controller/HomeController.php
@@ -26,10 +26,10 @@ class HomeController extends Controller
     $result = '{"code":0,"message":"succeed!",
                 "result":{
                   "list":[
-                    {"id":1,"url":"http://pic.58pic.com/58pic/14/13/09/64b58PICYzy_1024.jpg"},
-                    {"id":2,"url":"http://pic2.ooopic.com/10/57/50/93b1OOOPIC4d.jpg"},
-                    {"id":3,"url":"http://pic.58pic.com/58pic/14/13/09/64b58PICYzy_1024.jpg"},
-                    {"id":4,"url":"http://pic2.ooopic.com/10/57/50/93b1OOOPIC4d.jpg"}
+                    {"id":1,"url":"http://www.xuekaotong.cn/assets/images/mobile/banner/banner-1.jpg"},
+                    {"id":2,"url":"http://www.xuekaotong.cn/assets/images/mobile/banner/banner-2.jpg"},
+                    {"id":3,"url":"http://www.xuekaotong.cn/assets/images/mobile/banner/banner-3.jpg"},
+                    {"id":4,"url":"http://www.xuekaotong.cn/assets/images/mobile/banner/banner-4.jpg"}
                   ]
                 }
                }';
@@ -41,13 +41,13 @@ class HomeController extends Controller
     $result = '{"code":0,"message":"succeed!",
                 "result":{
                   "list":[
-                    {"id":1,"title":"注册建造师1","icon":"null","category_id":"207470","click_url":"http://www.xuekaotong.cn/api/school/categories/207470"},
-                    {"id":2,"title":"注册建造师2","icon":"null","category_id":"207470","click_url":"http://www.xuekaotong.cn/api/school/categories/207470"},
-                    {"id":3,"title":"注册建造师3","icon":"null","category_id":"207470","click_url":"http://www.xuekaotong.cn/api/school/categories/207470"},
-                    {"id":4,"title":"注册建造师4","icon":"null","category_id":"207470","click_url":"http://www.xuekaotong.cn/api/school/categories/207470"},
-                    {"id":5,"title":"注册建造师5","icon":"null","category_id":"207470","click_url":"http://www.xuekaotong.cn/api/school/categories/207470"},
-                    {"id":6,"title":"注册建造师6","icon":"null","category_id":"207470","click_url":"http://www.xuekaotong.cn/api/school/categories/207470"},
-                    {"id":7,"title":"注册建造师7","icon":"null","category_id":"207470","click_url":"http://www.xuekaotong.cn/api/school/categories/207470"},
+                    {"id":1,"title":"注册建造师","icon":"null","category_id":"207470","click_url":"http://www.xuekaotong.cn/api/school/categories/207470"},
+                    {"id":2,"title":"造价工程师","icon":"null","category_id":"207512","click_url":"http://www.xuekaotong.cn/api/school/categories/207512"},
+                    {"id":3,"title":"注册结构师","icon":"null","category_id":"209731","click_url":"http://www.xuekaotong.cn/api/school/categories/209731"},
+                    {"id":4,"title":"注册监理师","icon":"null","category_id":"209734","click_url":"http://www.xuekaotong.cn/api/school/categories/209734"},
+                    {"id":5,"title":"注册消防师","icon":"null","category_id":"209747","click_url":"http://www.xuekaotong.cn/api/school/categories/209747"},
+                    {"id":6,"title":"注册电气师","icon":"null","category_id":"209749","click_url":"http://www.xuekaotong.cn/api/school/categories/209749"},
+                    {"id":7,"title":"物业管理师","icon":"null","category_id":"209904","click_url":"http://www.xuekaotong.cn/api/school/categories/209904"},
                     {"id":8,"title":"更多..","icon":"null","category_id":"null","click_url":"null"}
                   ]
                 }
@@ -60,13 +60,13 @@ class HomeController extends Controller
     $result = '{"code":0,"message":"succeed!",
                 "result":{
                   "list":[
-                    {"id":1,"title":"一级建造师1","icon":"null","category_id":"207470","click_url":"http://www.xuekaotong.cn/api/school/categories/207470","teacher":"甘霖"},
-                    {"id":2,"title":"一级建造师2","icon":"null","category_id":"207470","click_url":"http://www.xuekaotong.cn/api/school/categories/207470","teacher":"甘霖"},
-                    {"id":3,"title":"一级建造师3","icon":"null","category_id":"207470","click_url":"http://www.xuekaotong.cn/api/school/categories/207470","teacher":"甘霖"},
-                    {"id":4,"title":"一级建造师4","icon":"null","category_id":"207470","click_url":"http://www.xuekaotong.cn/api/school/categories/207470","teacher":"甘霖"},
-                    {"id":5,"title":"一级建造师5","icon":"null","category_id":"207470","click_url":"http://www.xuekaotong.cn/api/school/categories/207470","teacher":"甘霖"},
-                    {"id":6,"title":"一级建造师6","icon":"null","category_id":"207470","click_url":"http://www.xuekaotong.cn/api/school/categories/207470","teacher":"甘霖"},
-                    {"id":7,"title":"一级建造师7","icon":"null","category_id":"207470","click_url":"http://www.xuekaotong.cn/api/school/categories/207470","teacher":"甘霖"},
+                    {"id":1,"title":"2015年一级建造师市政工程精讲课","icon":"null","category_id":"966796","click_url":"http://www.xuekaotong.cn/api/school/categories/966796","teacher":"甘 森"},
+                    {"id":2,"title":"2015一建建筑实务真题解析","icon":"null","category_id":"973411","click_url":"http://www.xuekaotong.cn/api/school/categories/207470","teacher":"韩 雷"},
+                    {"id":3,"title":"2014年真题一建市政肖国祥真题解析班01","icon":"null","category_id":"964807","click_url":"http://www.xuekaotong.cn/api/school/categories/207470","teacher":"肖国祥"},
+                    {"id":4,"title":"一建建筑城镇道路基层施工","icon":"null","category_id":"966791","click_url":"http://www.xuekaotong.cn/api/school/categories/207470","966791":"甘 森"},
+                    {"id":5,"title":"2015一建建筑实务真题解析","icon":"null","category_id":"972741","click_url":"http://www.xuekaotong.cn/api/school/categories/972741","teacher":"韩 雷"},
+                    {"id":6,"title":"2014年真题一建市政真题解析班","icon":"null","category_id":"966006","click_url":"http://www.xuekaotong.cn/api/school/categories/966006","teacher":"肖国祥"},
+                    {"id":7,"title":"2015年一级建造师市政精选课程二建-市政-精讲班17","icon":"null","category_id":"967758","click_url":"http://www.xuekaotong.cn/api/school/categories/967758","teacher":"曹明铭"},
                     {"id":8,"title":"更多..","icon":"null","category_id":"null","click_url":"null"}
                   ]
                 }

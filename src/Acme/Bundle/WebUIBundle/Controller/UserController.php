011f362fd4b353dc3fb38947958b35526324a030
zzpf <zzpf@163.com>
2016-01-18 14:26:50 +0800

    modify homepage layout, add index to homebase layout

---
+++ src/Acme/Bundle/WebUIBundle/Controller/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Acme\Bundle\WebUIBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use AppBundle\Utility\WebUtility\WebJson;
+use AppBundle\Utility\WebApi\WebResponse\CategoryChildResponse;
+use AppBundle\Utility\WebApi\WebResponse\CategoryRootResponse;
+
+class UserController extends Controller
+{
+    /**
+     * @Template()
+     */
+    public function profileAction(Request $request)
+    {
+
+    }
+
+}
+

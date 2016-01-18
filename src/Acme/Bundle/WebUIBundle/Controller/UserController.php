9efe0e9bbfeee49c49c00105509e27e8b5e22313
zzpf <zzpf@163.com>
2016-01-18 16:34:12 +0800

    fix the registration issue, add login status, add account profile page

--- src/Acme/Bundle/WebUIBundle/Controller/UserController.php
+++ src/Acme/Bundle/WebUIBundle/Controller/UserController.php
@@ -21,5 +21,8 @@ class UserController extends Controller
 
     }
 
+    /**
+     * 
+     */
 }
 
---
+++ src/Acme/Bundle/WebUIBundle/EventListener/RegistrationConfirmListener.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Acme\Bundle\WebUIBundle\EventListener;
+
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\FormEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+/**
+ * Listener responsible to change the redirection at the end of the 
+ */
+class RegistrationConfirmListener implements EventSubscriberInterface
+{
+    private $router;
+
+    public function __construct(UrlGeneratorInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            FOSUserEvents::REGISTRATION_CONFIRM => 'onRegistrationConfirm',
+        );
+    }
+
+    public function onRegistrationConfirm(GetResponseUserEvent $event)
+    {
+        $url = $this->router->generate('root_url');
+
+        $event->setResponse(new RedirectResponse($url));
+    }
+}
